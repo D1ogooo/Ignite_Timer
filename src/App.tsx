277f266f47378484aa7883745c93a43ad2561396
D1ogooo <diiogocosta11@gmail.com>
2023-10-11 22:36:59 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button'
+
+function App() {
+ return (
+  <>
+   <Button variant='primary'/>
+   <Button variant='secondary'/>
+   <Button variant='sucess'/>
+   <Button variant='danger'/>
+  </>
+ )
+}
+
+export default App
